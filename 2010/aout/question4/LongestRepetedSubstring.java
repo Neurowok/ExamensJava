feefc31da202ed72137d622624be92cd744bdf44
Neurowok <neurowok42@gmail.com>
2017-06-03 16:48:02 +0200

    test

--- 2010/aout/question4/LongestRepetedSubstring.java
+++ 2010/aout/question4/LongestRepetedSubstring.java
@@ -1,7 +1,6 @@
 public class LongestRepetedSubstring {
-	//
   public static String longestCommonPrefixe(String s1, String s2){
-	  String s = "";		
+		String s = "";		
 		int min = Math.min(s1.length(), s2.length());
 		for(int i = 0; i < min; i++){
 			if(s1.charAt(i) != s2.charAt(i)){
